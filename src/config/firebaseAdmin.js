329836d18d492b63a6b052573a051f1551f04c04
Harsh Singh <hs6262475@gmail.com>
2026-03-01 16:36:17 +0530

    Fixed firebase production config

--- src/config/firebaseAdmin.js
+++
@@ -1,9 +0,0 @@
-import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.js"; // JS file ko import kar rahe hain
-
-// Initialize Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-export default admin;
