93c75b2149f7237903441944650bcfca92182ad4
Harsh Singh <hs6262475@gmail.com>
2026-03-02 14:24:10 +0530

    Added new folder and updated files

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -14,6 +14,7 @@ import admin from "firebase-admin";
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
   "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  "universe_domain": "googleapis.com"
 }
 
 if (!admin.apps.length) {
