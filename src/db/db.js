b57d4a78ee5db9f440b6f1159cc4ff8c959fb017
Harsh Singh <hs6262475@gmail.com>
2026-04-28 07:49:41 -0700

    db file change

--- src/db/db.js
+++ src/db/db.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log("MongoDB Connected:", conn.connection.host);
-  } catch (error) {
-    console.error("FULL ERROR:", error);
-    process.exit(1);
-  }
-};
+    try {
+    const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log(`\n MongoDb connected !! DB HOST: ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("Mongo Db Connection error", error)
+        process.exit(1)
+    }
+}
 
 export default connectDB;
